5823d1519d4a2e0700d08b0f6b025f1119dbda78
Chris Siannas <xrsia@otenet.gr>
2022-01-04 14:46:15 +0200

    Modify a bit

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 	"strings"
 )
 
-func main(){
-	reader:=bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
-	prompt()
-	userInput, _ := reader.ReadString('\n')
-	userInput = strings.Replace(userInput,"\n","",-1)
+var reader *bufio.Reader
 
-	fmt.Println("Your name is", userInput)
+func main(){
+	reader=bufio.NewReader(os.Stdin)
+	userName:= readString("What is your name?")
+
+	fmt.Println("Your name is", userName)
 }
 
 func prompt() {
 	fmt.Print("-> ")
+}
+
+func readString(s string) string {
+	fmt.Println(s)
+	prompt()
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput,"\n","",-1)
+
+	return userInput
 }
