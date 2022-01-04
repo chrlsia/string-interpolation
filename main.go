9e3ee4a352636d9c187387814788e4aa2a98b1ff
Chris Siannas <xrsia@otenet.gr>
2022-01-04 14:38:32 +0200

    Func prompt

--- main.go
+++ main.go
@@ -10,9 +10,13 @@ import (
 func main(){
 	reader:=bufio.NewReader(os.Stdin)
 	fmt.Println("What is your name?")
-	fmt.Print("-> ")
+	prompt()
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.Replace(userInput,"\n","",-1)
 
 	fmt.Println("Your name is", userInput)
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
