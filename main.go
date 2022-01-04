bbb346c56f4a3ab86efdf1da91252fb09b94f5b7
Chris Siannas <xrsia@otenet.gr>
2022-01-04 15:06:02 +0200

    Ok except string interpolation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,8 +13,9 @@ var reader *bufio.Reader
 func main(){
 	reader=bufio.NewReader(os.Stdin)
 	userName:= readString("What is your name?")
+	age:=readInt("How old are you?")
 
-	fmt.Println("Your name is", userName)
+	fmt.Println("Your name is", userName,", and you are", age, "years old")
 }
 
 func prompt() {
@@ -21,10 +23,36 @@ func prompt() {
 }
 
 func readString(s string) string {
-	fmt.Println(s)
-	prompt()
-	userInput, _ := reader.ReadString('\n')
-	userInput = strings.Replace(userInput,"\n","",-1)
+	for {
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput,"\n","",-1)
 
-	return userInput
+		if userInput != "" {
+			return userInput
+		} else{
+			fmt.Println("Please enter a value")
+		}
+
+		
+	}
+}
+
+func readInt(s string) int {
+	for {
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput,"\n","",-1)
+
+		num, err:= strconv.Atoi(userInput)
+
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else {
+			return num	
+		}
+	}
+	
 }
